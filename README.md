69631ecab7ce17fa4a149e665dbaa2e6f04b6924
Matt Ward <ward.matt@gmail.com>
2017-03-19 20:32:59 +0000

    Add empty addin

---
+++ README.md
@@ -0,0 +1,4 @@
+# Open Any Folder Support for MonoDevelop and Xamarin Studio
+
+Provides the ability to open any folder instead of having to create a project.
+
---
+++ src/MonoDevelop.OpenAnyFolder/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin (
+	"MonoDevelop.OpenAnyFolder",
+	Namespace = "MonoDevelop.OpenAnyFolder",
+	Version = "0.1"
+)]
+
+[assembly: AddinName ("Open Any Folder")]
+[assembly: AddinCategory ("IDE extensions")]
+[assembly: AddinDescription ("MonoDevelop.OpenAnyFolder")]
+[assembly: AddinAuthor ("Matt Ward")]
